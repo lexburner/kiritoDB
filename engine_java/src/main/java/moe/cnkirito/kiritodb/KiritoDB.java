cec90825d6ca6bc86f533f0ce96fa6b53e80b2ff
kirito <25057914@qq.com>
2018-11-14 01:06:13 +0800

    '64mod'

--- engine_java/src/main/java/moe/cnkirito/kiritodb/KiritoDB.java
+++ engine_java/src/main/java/moe/cnkirito/kiritodb/KiritoDB.java
@@ -3,6 +3,7 @@ package moe.cnkirito.kiritodb;
 import com.alibabacloud.polar_race.engine.common.AbstractVisitor;
 import com.alibabacloud.polar_race.engine.common.exceptions.EngineException;
 import moe.cnkirito.kiritodb.common.Constant;
+import moe.cnkirito.kiritodb.common.Util;
 import moe.cnkirito.kiritodb.data.CommitLog;
 import moe.cnkirito.kiritodb.data.CommitLogAware;
 import moe.cnkirito.kiritodb.index.CommitLogIndex;
@@ -23,8 +24,8 @@ import java.util.concurrent.Executors;
 public class KiritoDB {
 
     private static final Logger logger = LoggerFactory.getLogger(KiritoDB.class);
-    private final int commitLogNum = 1 << 8; //64
-    private final int indexNum = 1 << 8; //64
+    private final int commitLogNum = 1 << 6; //64
+    private final int indexNum = 1 << 6; //64
     // 用于获取 key 的分区
     private Partitionable partitionable;
     private CommitLog[] commitLogs;
@@ -77,6 +78,7 @@ public class KiritoDB {
         CommitLogIndex hitIndex = commitLogIndices[partition];
         Long offset = hitIndex.read(key);
         if (offset == null) {
+            logger.error("key:{},partition:{}", Util.bytes2Long(key), partition);
             throw Constant.keyNotFoundException;
         }
         try {
--- engine_java/src/main/java/moe/cnkirito/kiritodb/partition/FirstBytePartitoner.java
+++ engine_java/src/main/java/moe/cnkirito/kiritodb/partition/FirstBytePartitoner.java
@@ -1,11 +1,13 @@
 package moe.cnkirito.kiritodb.partition;
 
+import moe.cnkirito.kiritodb.common.Util;
+
 /**
  * 根据 key 的第一个字节做分区，因为评测的 key 是从 0 ~ Long.MAX_VALUE 均匀分布的
  */
 public class FirstBytePartitoner implements Partitionable {
     @Override
     public int getPartition(byte[] key) {
-        return (key[0]) & 0xff;
+        return (int)(Math.abs(Util.bytes2Long(key)) / 64);
     }
 }
