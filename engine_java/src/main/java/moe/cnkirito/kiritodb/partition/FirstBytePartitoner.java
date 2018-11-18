15c1ef783dcc8efcd6d3a449b354a0be4be39e12
kirito <25057914@qq.com>
2018-11-19 04:04:09 +0800

    '1024真'

--- engine_java/src/main/java/moe/cnkirito/kiritodb/partition/FirstBytePartitoner.java
+++ engine_java/src/main/java/moe/cnkirito/kiritodb/partition/FirstBytePartitoner.java
@@ -6,6 +6,6 @@ package moe.cnkirito.kiritodb.partition;
 public class FirstBytePartitoner implements Partitionable {
     @Override
     public int getPartition(byte[] key) {
-        return key[0] & 0xff;
+        return ((key[0] & 0xff) << 2) + (key[1] & 0x03);
     }
 }
