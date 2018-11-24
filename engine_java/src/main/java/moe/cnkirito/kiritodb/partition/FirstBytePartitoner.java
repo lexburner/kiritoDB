8b5185c94c36d557ba516b8207d46b5366341c7a
kirito <25057914@qq.com>
2018-11-24 14:47:10 +0800

    '1024'

--- engine_java/src/main/java/moe/cnkirito/kiritodb/partition/FirstBytePartitoner.java
+++ engine_java/src/main/java/moe/cnkirito/kiritodb/partition/FirstBytePartitoner.java
@@ -1,7 +1,5 @@
 package moe.cnkirito.kiritodb.partition;
 
-import moe.cnkirito.kiritodb.common.Util;
-
 /**
  * 根据 key 的第一个字节做分区，因为评测的 key 是从 0 ~ Long.MAX_VALUE 均匀分布的
  */
@@ -9,8 +7,8 @@ public class FirstBytePartitoner implements Partitionable {
     @Override
     public int getPartition(byte[] key) {
 //        return (int) ((Util.bytes2Long(key) >> (64 - 8)) + (256/2));
-//        return (key[0] & 0xff << 2) | ((key[1] & 0xff) >> 6);
-        return key[0] & 0xff;
+        return ((key[0] & 0xff) << 2) | ((key[1] & 0xff) >> 6);
+//        return key[0] & 0xff;
 //        int modulus = (int) ((keyL >> (64-log2(分组数)) + (分组数/2));
     }
 }
