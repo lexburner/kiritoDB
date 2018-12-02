4dd0814c7a05da27ace802218b4a51d1eb1a0ad0
kirito <25057914@qq.com>
2018-12-02 13:08:00 +0800

    '1024'

--- engine_java/src/main/java/moe/cnkirito/kiritodb/partition/HighTenPartitioner.java
+++ engine_java/src/main/java/moe/cnkirito/kiritodb/partition/HighTenPartitioner.java
@@ -3,6 +3,6 @@ package moe.cnkirito.kiritodb.partition;
 public class HighTenPartitioner implements Partitionable {
     @Override
     public int getPartition(byte[] key) {
-        return ((key[0] & 0xff) << 3) | ((key[1] & 0xff) >> 5);
+        return ((key[0] & 0xff) << 2) | ((key[1] & 0xff) >> 6);
     }
 }
